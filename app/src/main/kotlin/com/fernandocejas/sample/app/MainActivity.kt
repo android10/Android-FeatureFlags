3a619d47f8c8071870577910812c073bce1e4219
Fernando Cejas <fcejas@gmail.com>
2020-03-27 14:34:38 +0100

    Add toolbar and proper style.

--- app/src/main/kotlin/com/fernandocejas/sample/app/MainActivity.kt
+++ app/src/main/kotlin/com/fernandocejas/sample/app/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.fernandocejas.sample.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+
+        val buildType = BuildConfig.BUILD_TYPE.toUpperCase(Locale.getDefault())
+        val productFlavor = BuildConfig.FLAVOR.toUpperCase(Locale.getDefault())
+        val appName = getString(R.string.app_name)
+        toolbar.title = "$appName: $buildType - $productFlavor"
     }
 }
